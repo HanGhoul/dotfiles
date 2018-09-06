93b69c0b2868326debc0c49962ee5df3d50b2165
HanGhoul <sec-github@dogil.in>
2018-09-06 12:03:35 +0200

    blabla

--- .config/polybar/scripts/bluetooth.sh
+++ .config/polybar/scripts/bluetooth.sh
@@ -3,9 +3,7 @@
 status=`bluetoothctl show | grep Powered | awk '{print $2}'`
 
 if [ $status = "yes" ]; then
-	icon="%{F#68A2DA}%{F-} %{F#cbd6e2}on%{F-}"
+	echo 1
 else
-	icon="%{F#f7f9fb}%{F-} %{F#cbd6e2}off%{F-}"
+	exit 1
 fi
-
-echo $icon
